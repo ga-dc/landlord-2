13e37a6e76a1f34c11a61360a11c81941dec2831
Andy <andrew.sunglae.kim@gmail.com>
2015-07-20 12:19:38 -0400

    change person to tenant

--- config/seeds.rb
+++ config/seeds.rb
@@ -4,9 +4,9 @@
 
 # create at least 9 objects(at least 5 must be long to an apartment)
 
-# query for all objects of the Person class, store it in a variable
+# query for all objects of the Tenant class, store it in a variable
 
-# query for all instances of the Person class that belong to the first Apartment you created
+# query for all instances of the Tenant class that belong to the first Apartment you created
 
 # update any one of your objects you've created using attribute helper methods
 
