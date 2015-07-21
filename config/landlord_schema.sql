edb13ff86aa7f9dbb6cc7868a14f0284c66b1b7b
Rachel Warren <raelwa@gmail.com>
2015-07-21 17:25:08 -0400

    was able to load schema in terminal

--- config/landlord_schema.sql
+++ config/landlord_schema.sql
@@ -4,16 +4,16 @@ DROP TABLE IF EXISTS apartments;
 CREATE TABLE tenants (
   id SERIAL PRIMARY KEY,
   name TEXT,
-  age TEXT,
+  age INT,
   gender TEXT,
-  apartment_id
+  apartment_id INT
 );
 
 CREATE TABLE apartments (
   id SERIAL PRIMARY KEY,
-  address TEXT
-  monthly_rent INT
-  sqft INT
-  num_beds INT
+  address TEXT,
+  monthly_rent INT,
+  sqft INT,
+  num_beds INT,
   num_baths INT
 );
