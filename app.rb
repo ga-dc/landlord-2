17ed61679991492575c81318fd06157d2e162735
Alex Bannon <alexbannon@gmail.com>
2015-07-21 22:57:20 -0400

    ultra bonus working. Added tenant-side features such as relocate, evict, delete

--- app.rb
+++ app.rb
@@ -53,7 +53,7 @@ while userInput != "q"
         puts "#{vacant_bedrooms + 1}. Vacant"
         vacant_bedrooms += 1
       end
-      puts "** Enter Vacant Number To Lease to Tenant **"
+      puts "** Enter Slot Number To Change Tenant **"
       puts "** s to Sell Apartment **"
       puts "** q to Quit, r to Return to Main **"
       apartment_change = gets.chomp
@@ -76,15 +76,16 @@ while userInput != "q"
           end
           vacant_to_tenant = gets.chomp
           Tenant.all[((vacant_to_tenant.to_i) -1)].update_attributes(apartment_id: Apartment.find_by(address: save_address).id)
-        # else
-        #   puts "Who do you want to replace #{temp.tenants[apartment_change.to_i - 1].name}?"
-        #   Tenant.all.each_with_index do |tenant, i|
-        #     if tenant.name != temp.tenants[apartment_change.to_i - 1].name
-        #       puts "#{i+1}. #{tenant.name}"
-        #     end
-        #   end
-        #   vacant_to_tenant = gets.chomp
-        #   Tenant.all[((vacant_to_tenant.to_i) -1)].update_attributes(apartment_id: Apartment.find_by(address: save_address).id)
+        else
+          puts "Who do you want to replace #{temp.tenants[apartment_change.to_i - 1].name}? (use ID)"
+          Tenant.all.each do |tenant|
+            if tenant.name != temp.tenants[apartment_change.to_i - 1].name
+              puts "ID: #{tenant.id} Name: #{tenant.name}"
+            end
+          end
+          temp.tenants[apartment_change.to_i - 1].update_attributes(apartment_id: nil)
+          user_tenant_id = gets.chomp
+          Tenant.find_by(id: user_tenant_id).update_attributes(apartment_id: Apartment.find_by(address: save_address).id)
         end
       end
     end
@@ -93,7 +94,7 @@ while userInput != "q"
     Tenant.all.each_with_index do |tenant, i|
       puts "#{i+1}. #{tenant.name}"
     end
-    # puts "** Enter Tenant Number to Modify/Evict **"
+    puts "** Enter Tenant Number to Modify/Evict **"
     puts "** Enter a to Add Tenant **"
     puts "** Enter r to Return to Main Menu **"
     puts "** Enter q to Quit **"
@@ -118,7 +119,43 @@ while userInput != "q"
     when "r"
       userInput = "run"
     else
-      # puts "not working yet"
+      puts "Tenant Name: #{Tenant.all[((tenant_choice.to_i) - 1)].name}"
+      if Tenant.all[((tenant_choice.to_i) - 1)].apartment_id != nil
+        puts "Location: #{Tenant.all[((tenant_choice.to_i) - 1)].apartment.address}"
+      else
+        puts "Location: Not Residing In Your Units"
+      end
+      temp_tenant_id = Tenant.all[((tenant_choice.to_i) - 1)].id
+      puts "** Enter e for Evict **"
+      puts "** Enter r for Relocate **"
+      puts "** OR enter q to Quit or m for Main Menu"
+      user_input_tenant_change = gets.chomp
+      case user_input_tenant_change
+      when "q"
+        userInput = "q"
+      when "m"
+        userInput = "run"
+      when "e"
+        if Tenant.find(temp_tenant_id).apartment_id == nil
+          puts "Tenant is not residing in any of your units. Good thing, I guess!"
+        else
+          Tenant.find(temp_tenant_id).update_attributes(apartment_id: nil)
+          puts "Tenant evicted!"
+        end
+        puts "Do you want to permanently delete tenant record? y/n"
+        tenant_delete = gets.chomp
+        if tenant_delete == "y"
+          Tenant.find(temp_tenant_id).destroy
+        end
+      when "r"
+        temp_tenant_id = Tenant.all[((tenant_choice.to_i) - 1)].id
+        puts "Which Apartment ID Do You Want to Place #{Tenant.find(temp_tenant_id).name}? (numbers only):"
+        Apartment.all.each do |apartment|
+          puts "#{apartment.id}. #{apartment.address}"
+        end
+        new_tenant_apartment = gets.chomp.to_i
+        Tenant.find(temp_tenant_id).update_attributes(apartment_id: new_tenant_apartment)
+      end
     end
   end
 end
