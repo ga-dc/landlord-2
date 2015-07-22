424984ef5a5d7b3646e5b7c1bd5a598389e1c7a9
MikeDulik <mikedulik@gmail.com>
2015-07-22 18:16:26 -0400

    tried modelling after class examples, then tried modelling after solution branch code, and cant get instances of created objects to pass into database... tested seed code on class examples to make sure methods yield correct answers

---
+++ app.rb
@@ -0,0 +1,10 @@
+#require_relative "config/setup"
+
+require 'active_record'
+require 'pry'
+
+require_relative 'models/apartment'
+require_relative 'models/tenant'
+require_relative 'config/db.rb'
+
+binding.pry
