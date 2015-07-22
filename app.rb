a55152de2ff9f92ccbff90d6696e82371ee24a2b
Melissa Steffan <msteffan2012@gmail.com>
2015-07-21 22:50:39 -0400

    solved bonus and some of the mega bonus I guess

--- app.rb
+++ app.rb
@@ -19,16 +19,16 @@ def get_input(prompt)
 end
 
 def menu
-    puts "Hello! Welcome to the LandLine — a landlord command line tool! What would you like to do?"
+    puts "Welcome to the LandLine — a landlord command line tool! Main Menu: What would you like to do?"
     puts "Options are:"
-    puts "1. View apartments"
-    puts "2. View current tenants"
+    puts "1. View apartments by address"
+    puts "2. View current tenants by name"
     puts "3. Create a new apartment"
-    puts "4. Create a new tenant"
-    puts "To make your selection, type 1, 2, 3, or 4, then press the 'enter' key."
+    puts "4. Change an existing apartment's rent"
+    puts "5. Create a new tenant"
+    puts "6. Update a tenant's residence/apartment"
+    puts "To make your selection, type 1-6, then press the 'enter' key."
 end
-
-
 
 in_menu = true
 while in_menu
@@ -54,12 +54,24 @@ while in_menu
         Apartment.create(address: address, monthly_rent: monthly_rent.to_i, sqft: sqft.to_i, num_beds: num_beds.to_i, num_baths: num_baths.to_i)
         puts "Your new apartment has been added!"
     elsif landlord_input == "4"
+        puts "Let's change the rent."
+        apt_address = get_input("What is the address of the apartment that you would like to update?")
+        new_rent = get_input("What is the new montly rent?")
+        Apartment.find_by(address: apt_address).update(monthly_rent: new_rent)
+        puts "The rent has been updated."
+    elsif landlord_input == "5"
         puts "Let's add a new tenant."
         name = get_input("What is the tenant's name?")
         age = get_input("What is the tenant's age?")
         gender = get_input ("What is the tenant's gender? Enter 'm' or 'f'")
         Tenant.create(name: name, age: age.to_i, gender: gender)
         puts "Your new tenant has been added!"
+    elsif landlord_input == "6"
+        puts "Let's change a tenant's residence."
+        tenant_name = get_input("Enter the full name of the tenant you'd like to update.")
+        apt_address = get_input("Which apartment does this tenant currently occupy? Please identify the apartment by address.")
+        update_address = Apartment.find_by(address: apt_address).id
+        Tenant.find_by(name: tenant_name).update(apartment_id: update_address)
+        puts tenant_name + "'s apartment has been updated."
     end
-    puts "back to the main menu!"
 end
