1cf83a4d1a0fbbb2681ff21c4fc2e52d1a711d98
Karl Sakas <donusdonut@Karls-MBP.home>
2015-07-22 23:10:58 -0400

    database set up

--- config/seeds.rb
+++ config/seeds.rb
@@ -1,8 +1,22 @@
-# don't forget to load dependencies!
+require_relative 'setup'
 
-# create at least 3 objects of the apartment class
+Apartment.destroy_all
+Tenant.destroy_all
 
-# create at least 9 objects(at least 5 must be long to an apartment)
+deerfield = Apartment.create(address: "8716 Selger Drive", monthly_rent: "$3500", sqft: 2400, num_beds: 6, num_baths: 2)
+rivermont = Apartment.create(address: "2306 Rivermont Ave", monthly_rent: "$1500", sqft: 2000, num_beds: 4, num_baths: 3)
+randolph = Apartment.create(address: "2500 Rivermont Ave", monthly_rent: "$500", sqft: 144, num_beds: 2, num_baths: 2)
+
+
+Tenant.create(last_name: "Ride", first_name: "Sally", age: 33, gender: "female", apartment: deerfield)
+Tenant.create(last_name: "Gagarin", first_name: "Yuri", age: 35, gender: "male", apartment: rivermont)
+Tenant.create(last_name: "Glenn", first_name: "John", age: 31, gender: "male", apartment: randolph)
+Tenant.create(last_name: "Armstrong", first_name: "Neil", age: 44, gender: "male", apartment: randolph)
+Tenant.create(last_name: "Sheppard", first_name: "Alan", age: 45, gender: "male", apartment: deerfield)
+Tenant.create(last_name: "Aldrin", first_name: "Hadfield", age: 37, gender: "male", apartment: deerfield)
+Tenant.create(last_name: "Hatfield", first_name: "Chris", age: 42, gender: "male")
+Tenant.create(last_name: "Lightyear", first_name: "Buzz", age: 22, gender: "male")
+Tenant.create(last_name: "Lovell", first_name: "Jim", age: 51, gender: "male")
 
 # query for all objects of the Tenant class, store it in a variable
 
