f0b580f314f0c7747fc7f313f79a5bce8d7439ce
John Master <john.s.master@gmail.com>
2015-07-22 23:10:44 -0400

    Create apartments in seeds

--- config/seeds.rb
+++ config/seeds.rb
@@ -5,8 +5,12 @@ Tenant.destroy_all
 # don't forget to load dependencies!
 
 # create at least 3 objects of the apartment class
-
-# create at least 9 objects(at least 5 must be long to an apartment)
+park_supervisor_lot = Apartment.create(address: "Lot 1", monthly_rent: 0, sqft: 4000, num_beds: 3, num_baths: 2)
+Apartment.create(address: "Lot 12", monthly_rent: 500, sqft: 2000, num_beds: 2, num_baths: 1)
+Apartment.create(address: "Lot 24", monthly_rent: 500, sqft: 2000, num_beds: 2, num_baths: 1)
+Apartment.create(address: "Julian's Car", monthly_rent: 10, sqft: 20, num_beds: 1, num_baths: 1)
+Apartment.create(address: "Bubbles' Shed", monthly_rent: 200, sqft: 200, num_beds: 2, num_baths: 0)
+# create at least 9 objects(at least 5 must belong to an apartment)
 
 # query for all objects of the Tenant class, store it in a variable
 
