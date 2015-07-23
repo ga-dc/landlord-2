1aa19a87d7d9e94a203640dc95df637592ac5017
John Master <john.s.master@gmail.com>
2015-07-22 23:58:58 -0400

    add seeds

--- config/seeds.rb
+++ config/seeds.rb
@@ -11,15 +11,23 @@ Apartment.create(address: "Lot 24", monthly_rent: 500, sqft: 2000, num_beds: 2, 
 Apartment.create(address: "Julian's Car", monthly_rent: 10, sqft: 20, num_beds: 1, num_baths: 1)
 Apartment.create(address: "Bubbles' Shed", monthly_rent: 200, sqft: 200, num_beds: 2, num_baths: 0)
 # create at least 9 objects(at least 5 must belong to an apartment)
-
-# query for all objects of the Tenant class, store it in a variable
-
-# query for all instances of the Tenant class that belong to the first Apartment you created
-
-# update any one of your objects you've created using attribute helper methods
-
-# save that object you just updated to the database
-
-# update an object using the update method
-
+park_supervisor_lot.tenants.create(name: "Mr. Leahy", age: 60, gender: "Male")
+park_supervisor_lot.tenants.create(name: "Randy", age: 35, gender: "Male")
+Tenant.create(name: "Bubbles", age: 28, gender: "Male", apartment_id: Apartment.find_by(address: "Bubbles' Shed").id)
+Tenant.create(name: "Cyrus", age: 40, gender: "Male", apartment: Apartment.find_by(address: "Lot 12"))
+julian = Tenant.new(name: "Julian", age: 30, gender: "Male")
+Tenant.create(name: "Ricky", age: 30, gender: "Male")
+Apartment.find_by(address: "Lot 12").tenants = [julian] #This adds Julian to the database which makes sense but I wasn't expecting
 # delete an object
+Tenant.find_by(name: "Cyrus").destroy
+# update an object using the update method
+Tenant.find_by(name: "Ricky").update(apartment: Apartment.find_by(address: "Julian's car"))
+(1..3).each {|num| Apartment.find_by(num_baths: 0).tenants.create(name: "Cat No. #{4 - num}", age: (num * 7), gender: "?")}
+# update any one of your objects you've created using attribute helper methods
+julian.age = 31
+# save that object you just updated to the database
+julian.save
+# query for all objects of the Tenant class, store it in a variable
+tenants = Tenant.all
+# query for all instances of the Tenant class that belong to the first Apartment you created
+love_birds = Tenant.where(apartment: Apartment.first)
